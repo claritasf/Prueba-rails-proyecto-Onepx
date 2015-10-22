83e7e69b2ab1a37be0111457491655fc0867534c
Clarita Silva <claritasf@gmail.com>
2015-10-21 20:14:47 -0500

    Relacion de modelo identity con image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ActiveRecord::Base
 	mount_uploader :photo, PhotoUploader
 
+	belongs_to :identity
+
 	enum category: %w(portrait landscape city\ exploration nature animals) #toma las posiciones que tenemos en category y determina o asigna cuales son los valores
 
 	validates :name, presence: true #name tiene que ser requerido
